c94d448743fbcc30b7c4f828d7a5ebae1230f403
Amir213902 <amir.emkuzhev09@gmail.com>
2026-01-15 12:28:04 +0300

    Исправление ошибки

--- src/pages/Board.page.jsx
+++ src/pages/Board.page.jsx
@@ -1,17 +1,9 @@
 import MyGrid from '../components/MyGrid'
-import MyResponsiveGrid from '../components/MyResponsiveGrid'
-import Device from 'device.js'
-
-const deviceInfo = new Device()
 
 function BoardPage() {
 
     return (
-        deviceInfo.desktop ? (
-            <MyGrid />
-        ) : (
-            <MyResponsiveGrid />
-        )
+        <MyGrid />
     )
 }
 
